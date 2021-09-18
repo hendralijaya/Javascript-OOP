88ba0531bdf64e245645ab9a9f884bb2112a0346
Hael12122002 <hendralijaya@gmail.com>
2021-09-18 16:29:52 +0700

    Menambahkan lesson 3

---
+++ lesson 3/objects.js
@@ -0,0 +1,32 @@
+let userOne = {
+  email: "budi@gmail.com",
+  name: "budi",
+  login() {
+    console.log(`${this.email} has logged in`);
+  },
+  logout() {
+    console.log(`${this.email} has logged out`);
+  },
+};
+
+let userTwo = {
+  email: "siti@gmail.com",
+  name: "siti",
+  login() {
+    console.log(`${this.email} has logged in`);
+  },
+  logout() {
+    console.log(`${this.email} has logged out`);
+  },
+};
+
+let userThree = {
+  email: "bagus@gmail.com",
+  name: "bagus",
+  login() {
+    console.log(`${this.email} has logged in`);
+  },
+  logout() {
+    console.log(`${this.email} has logged out`);
+  },
+};
